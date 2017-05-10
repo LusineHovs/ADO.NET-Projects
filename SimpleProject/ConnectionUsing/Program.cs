e178303b01867c2165848348fadf3f2d3d1c2a67
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-05-10 18:03:46 +0400

    Changes

---
+++ SimpleProject/ConnectionUsing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConnectionUsing
+{
+    class Program
+    {
+        static void connection_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            SqlConnection connection = sender as SqlConnection;
+            Console.WriteLine();
+
+            Console.WriteLine
+                (
+                "Connection to" + Environment.NewLine +
+                "Data Source: " + connection.DataSource + Environment.NewLine +
+                "Database: " + connection.Database + Environment.NewLine +
+                "State: " + connection.State
+                );
+        }
+        static void Main(string[] args)
+        {
+            string conStr = @"Data Source=(localdb)\MSSQLLocalDB;
+                              Initial Catalog = ShopDB;
+                              Integrated Security = True";
+
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                connection.StateChange += connection_StateChange;
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+        }
+    }
+}
