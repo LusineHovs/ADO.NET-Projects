03d5ef6d552bfbda3eece48f55f9042c7b3b1e22
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-05-10 17:21:09 +0400

    Changes

---
+++ SimpleProject/ConnectionStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = @"Data Source=(localdb)\MSSQLLocalDB;
+                              Initial Catalog = ShopDB;
+                              Integrated Security = True";
+            SqlConnection connection = new SqlConnection(conStr);
+            try
+            {
+                connection.Open();
+                Console.WriteLine(connection.State);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine(connection.State);
+            }
+        }
+    }
+}
