5c0658806d833fcc3e1faf646dc4285246cf2499
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-05-11 12:45:49 +0400

    Additions

--- SimpleProject/AppConfigCrypt/Program.cs
+++ SimpleProject/AppConfigCrypt/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace AppConfigCrypt
 {
@@ -10,6 +11,31 @@ namespace AppConfigCrypt
     {
         static void Main(string[] args)
         {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.ConnectionStrings.ConnectionStrings.Add(new ConnectionStringSettings("COnnectionStr1", "SomeConnectionString"));
+            config.Save();
+
+            ConnectionStringsSection section = config.GetSection("connectionStrings") as ConnectionStringsSection;
+
+            if (section.SectionInformation.IsProtected)
+            {
+                //connection string section decrypted
+                section.SectionInformation.UnprotectSection();
+            }
+            else
+            {
+                //connection string section crypted
+                section.SectionInformation.ProtectSection(
+                    "DataProtectionConfigurationProvider");
+            }
+
+            
+            config.Save();
+
+            // checkes crypting
+            Console.WriteLine("Protected={0}", section.SectionInformation.IsProtected);
+
+            Console.WriteLine(ConfigurationManager.ConnectionStrings["COnnectionStr1"].ConnectionString);
         }
     }
 }
---
+++ SimpleProject/OLEDB_Provider/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLEDB_Provider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectToAccessDB();
+            ConnectToExcelDB();
+        }
+
+        // data provider is Microsoft Access 
+        private static void ConnectToAccessDB()
+        {
+            var conn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0; Data Source= D:\ADO.NET\DATA\Access.mdb");
+
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Connection to .mdb(AccessDB) file opened successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                conn.Close();
+                Console.WriteLine("Connection closed");
+            }
+        }
+
+        // data provider is Microsoft Excel
+        private static void ConnectToExcelDB()
+        {
+            var conn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=D:\ADO.NET\DATA\Excel.xls; Extended Properties=""Excel 8.0""");
+
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Connection to .xls(ExcelDB) file opened successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                conn.Close();
+                Console.WriteLine("Connection closed");
+            }
+        }
+
+    }
+}
