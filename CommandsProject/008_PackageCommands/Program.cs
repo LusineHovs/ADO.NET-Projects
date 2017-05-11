9590612d4f2d00f185a8c6b1a46bd909f39bbdd9
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-05-11 17:30:48 +0400

    Additions

---
+++ CommandsProject/008_PackageCommands/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_PackageCommands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = @"Data Source = (localdb)\MSSQLLocalDB;
+                                            Initial Catalog = ShopDB;
+                                            Integrated Security = True";
+
+            SqlConnection connection = new SqlConnection(conStr);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Customers WHERE CustomerNo = 1; SELECT * FROM Employees WHERE EmployeeID = 1;", connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.WriteLine(reader.GetName(i)+ ": "+ reader[i]);
+                    
+                }
+                Console.WriteLine(new string('-', 20));
+            }
+
+            Console.ReadKey();
+
+
+            // calls the next Command
+            reader.NextResult();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.WriteLine(reader.GetName(i) + ": " + reader[i]);
+
+                }
+                Console.WriteLine(new string('-', 20));
+            }
+
+            reader.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ CommandsProject/009_DBColumnNullValue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009_DBColumnNullValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = @"Data Source = (localdb)\MSSQLLocalDB;
+                                            Initial Catalog = ShopDB;
+                                            Integrated Security = True";
+
+            SqlConnection connection = new SqlConnection(conStr);
+            SqlCommand cmd = new SqlCommand("select * from Customers", connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                if (reader.IsDBNull(5))
+                    Console.WriteLine("Address Line 2: " + "No Data");
+                else
+                    Console.WriteLine("Address Line 2: " +reader[5]);
+            }
+        }
+    }
+}
