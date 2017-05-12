27e148eeda8c988cdc3866e299125d7d398f94f7
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-05-12 16:31:45 +0400

    Additions

---
+++ ColumnTableProperites/001_ReadOnlyProperty/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_ReadOnlyProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable table = new DataTable();
+            DataColumn column = table.Columns.Add("ReadOnlyColumn", typeof(string));
+            column.ReadOnly = true;   // this column only for reading
+
+            DataRow row = table.NewRow();
+            row[0] = "readonlyValue";
+            table.Rows.Add(row);
+
+            Console.WriteLine(table.Rows[0][0]);
+
+           // table.Rows[][] = "newvalue";  => error
+        }
+    }
+}
---
+++ ColumnTableProperites/002_AllowDBNull/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_AllowDBNull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable table = new DataTable();
+            DataColumn column = table.Columns.Add("AllowDBNullColumn", typeof(int));
+            column.AllowDBNull = true;
+
+            DataRow row = table.NewRow();
+            row[0] = DBNull.Value;
+
+            table.Rows.Add(row);
+            Console.WriteLine(table.Rows[0][0]);
+        }
+    }
+}
