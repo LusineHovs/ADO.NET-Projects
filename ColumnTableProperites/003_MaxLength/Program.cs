8e5a91311217936d0704fc9ff6fdd081e30d9d89
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-05-12 16:44:40 +0400

    Additions in Columns constraints

---
+++ ColumnTableProperites/003_MaxLength/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_MaxLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable table = new DataTable();
+            DataColumn column = table.Columns.Add("MaxLengthConstraintColumn", typeof(string));
+            column.MaxLength = 5;
+
+            DataRow row = table.NewRow();
+            row[0] = "Some";
+            table.Rows.Add(row);
+            Console.WriteLine(table.Rows[0][0]);
+        }
+    }
+}
---
+++ ColumnTableProperites/004_UniqueProperty/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_UniqueProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable table = new DataTable();
+
+            DataColumn column = table.Columns.Add("UniqueColumn", typeof(string));
+            column.Unique = true;
+
+            DataRow newRow = table.NewRow();
+            newRow[0] = "NonUniqueValue";
+            table.Rows.Add(newRow);
+
+            newRow = table.NewRow();
+            newRow[0] = "NonUniqueValue";
+            table.Rows.Add(newRow); // ошибка времени выполнения при нарушении ограничения Unique
+
+            Console.WriteLine(table.Rows[0][0]);
+            Console.WriteLine(table.Rows[1][0]);
+        }
+    }
+}
